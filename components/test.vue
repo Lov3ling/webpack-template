81d87dda88cda89021748858bfd19af88558109a
lov3ling <2625899521@qq.com>
2017-10-09 16:50:33 +0800

    vue components

---
+++ components/test.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Example Component</div>
+
+                    <div class="panel-body">
+                        I'm an example pages
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Vue is run')
+        }
+    }
+</script>
+<style>
+    .container{
+        background-image: url(./images/bg.jpg);
+    }
+</style>
